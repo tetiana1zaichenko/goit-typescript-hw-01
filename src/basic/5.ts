d7425a149b0d20ead3a42ec66b926b96c91c11e6
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-05-17 19:40:49 +0300

    fixed basik-5

--- src/basic/5.ts
+++ src/basic/5.ts
@@ -1,15 +1,15 @@
-enum DayOfWeek {
-  Monday,
-  Tuesday,
-  Wednesday,
-  Thursday,
-  Friday,
-  Saturday,
-  Sunday,
-}
+const DayOfWeek = {
+  Monday: 0,
+  Tuesday: 1,
+  Wednesday: 2,
+  Thursday: 3,
+  Friday: 4,
+  Saturday: 5,
+  Sunday: 6,
+} as const;
+
+type DayOfWeek = (typeof DayOfWeek)[keyof typeof DayOfWeek];
 
 const isWeekend = (day: DayOfWeek): boolean => {
   return day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
 };
-
-export {};
